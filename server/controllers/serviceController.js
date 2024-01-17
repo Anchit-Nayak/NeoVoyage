c8a3e239b090490d73e22bc47e7047123e074c1b
Aniket Pawar <aniket@archstore.in>
2024-01-17 21:41:08 +0530

    getServices api created

--- server/controllers/serviceController.js
+++ server/controllers/serviceController.js
@@ -1,26 +1,26 @@
-const service = require('../models/service')
+const service = require("../models/service");
 
-const getServices = async (req,res) => {
-    try{
-        const { serviceType, sortByRate, location, all } = req.body;
-        if(all){
-            const randomRecords = await service.aggregate([
-                { $sample: { size: 20 } }
-              ]);
-          
-              console.log(randomRecords);
-              return res.json(randomRecords)
-        }
-        else {
-            const sortType = sortByRate === 'hightolow' ? -1 : 1;
-            const records = await service.find({type: serviceType,location}).sort({rating: sortType})
-            return res.json(records)
-        }
+const getServices = async (req, res) => {
+  try {
+    const { serviceType, sortByRate, location, all } = req.body;
+    if (all) {
+      const randomRecords = await service.aggregate([
+        { $sample: { size: 20 } },
+      ]);
 
-    } catch(err){
-        console.log(err)
-        res.status(500).json({message:"Internal Server error"})
+      console.log(randomRecords);
+      return res.json(randomRecords);
+    } else {
+      const sortType = sortByRate === "hightolow" ? -1 : 1;
+      const records = await service
+        .find({ type: serviceType, location })
+        .sort({ rating: sortType });
+      return res.json(records);
     }
-}
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server error" });
+  }
+};
 
-module.exports = { getServices }
+module.exports = { getServices };
