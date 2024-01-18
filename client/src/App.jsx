f401c7c33628180b059b11c8454b10ac72102710
Aniket Pawar <aniket@archstore.in>
2024-01-18 21:33:19 +0530

    created protected Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import Signin from './Pages/Signin/Signin';
 import Signup from './Pages/Signup/Signup';
 import Home from './Pages/Home/Home';
@@ -10,19 +10,49 @@ import Advisory from './Pages/Advisory/Advisory';
 import OtpVerification from './Pages/OtpVerification';
 import BuisnessSignup from './Pages/Signup/BuisnessSignup';
 
+const isAuthenticated = () => {
+  const accessToken = localStorage.getItem('accessToken');
+  return !!accessToken; // Returns true if there is an access token, false otherwise
+};
+const TouristRoute = ({ component: Component }) => {
+  if (isAuthenticated()) {
+    const type = localStorage.getItem("role");
+    if(type == "business"){
+      return <Navigate to="/signin" />
+    }
+    return <Component />;
+  } else {
+    // Redirect to the sign-in page if not authenticated
+    return <Navigate to="/signin" />;
+  }
+};
+
+const BusinessRoute = ({ component: Component }) => {
+  if (isAuthenticated()) {
+    const type = localStorage.getItem("role");
+    if(type == "tourist"){
+      return <Navigate to="/" />
+    }
+    return <Component />;
+  } else {
+    // Redirect to the sign-in page if not authenticated
+    return <Navigate to="/signin" />;
+  }
+};
+
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route index element={<Home/>} />
-        <Route path="/explore" element={<Explore/>} />
+        <Route path="/explore" element={<TouristRoute component={<Explore/>}/>} />
         <Route path="/compare" element={<Compare/>} />
         <Route path="/service/:id" element={<Service/>} />
         <Route path='/advisory' element={<Advisory/>}/>
       </Route>
       <Route path="/servicesignup" element={<BuisnessSignup/>} />
-      <Route path="verification" element={<OtpVerification/>}/>
+      <Route path="/verification" element={<OtpVerification/>}/>
       <Route path="/signin" element={<Signin/>} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
